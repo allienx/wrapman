24c5561441a663d80645fca0a894fa8c3dab2ca8
Alex Lin <allienx@hashmail.cc>
2024-02-15 22:40:19 -0800

    Bump v1.0.2

--- src/wrapman-cli.ts
+++ src/wrapman-cli.ts
@@ -10,7 +10,7 @@ runIt({
 
     program
       .name('wrapman')
-      .version('1.0.1')
+      .version('1.0.2')
       .description(
         'Postman API request collection wrapper that generates an axios http client.',
       )
