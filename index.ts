fde9ce6eaca50b641f2315b9087976d45c3c441a
Alex Lin <allienx@hashmail.cc>
2024-02-15 21:38:59 -0800

    Add main exports and declare bin executable

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
 export { Wrapman } from 'src/wrapman'
+export { WrapmanApiClient } from 'src/wrapman-api-client'
+export type {
+  WrapmanCollection,
+  WrapmanCollectionItem,
+} from 'src/wrapman-collection'
